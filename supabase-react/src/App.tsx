7bd0da56fd730ea65cc201420e147984924ad3b5
Ruben Oostinga <roostinga@xebia.com>
2023-09-29 10:09:00 +0200

    Add getting started api call

--- supabase-react/src/App.tsx
+++ supabase-react/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const supabase = createClient("https://pnmvlzlucrchefrrfpck.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubXZsemx1Y3JjaGVmcnJmcGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5NzQ1NDQsImV4cCI6MjAxMTU1MDU0NH0.CE8H2Njsaj2Rshk1fr0xgkUXO5ufQ2Qxn28JWazxzgU");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  async function getCountries() {
+    const { data } = await supabase.from("countries").select();
+    setCountries(data);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {countries.map((country) => (
+        <li key={country.name}>{country.name}</li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
